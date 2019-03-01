0c06b1a73bbcc96df4e5a7a31a85dc18356a3159
Rana <mittrarana@gmail.com>
2019-03-01 12:27:17 +0000

    task one complete

--- AutomatedTest/src/test/java/tests/AssessmentTest.java
+++ AutomatedTest/src/test/java/tests/AssessmentTest.java
@@ -9,7 +9,47 @@ import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import landingpages.JenkinsConf;
+import landingpages.JenkinsHome;
+import landingpages.JenkinsJobBoard;
+import landingpages.JenkinsLogin;
+import landingpages.JenkinsNewJob;
+
 public class AssessmentTest {
 	
+	private String pass = "admin";
+	private String jobName = "AutomatedTest";
+	
+	ChromeDriver driver;
+	@Before
+	public void setup() {
+		System.setProperty(Constants.DriverSetUp1, Constants.DriverSetUp2);
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void TaskOne() throws Exception {
+		driver.manage().window().maximize();
+		driver.get(Constants.Jenkins);
+		JenkinsLogin login = PageFactory.initElements(driver, JenkinsLogin.class);
+		login.login(pass);
+		JenkinsHome home = PageFactory.initElements(driver, JenkinsHome.class);
+		home.newItem();
+		JenkinsNewJob job = PageFactory.initElements(driver, JenkinsNewJob.class);
+		job.makeJob(jobName);
+		Thread.sleep(1000);
+		JenkinsConf conf = PageFactory.initElements(driver, JenkinsConf.class);
+		conf.saveConf();
+		JenkinsJobBoard jb = PageFactory.initElements(driver, JenkinsJobBoard.class);
+		jb.returnToDashboard();
+		assertEquals(jobName,home.checkFirstJob());
+		
+		
+	}
 	
 }
--- AutomatedTest/src/test/java/tests/Constants.java
+++ AutomatedTest/src/test/java/tests/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 	public static final String EXCEL = "C:\\Users\\Admin\\Downloads\\Parametisation.xlsx";
 	public static final String DriverSetUp1 = "webdriver.chrome.driver";
 	public static final String DriverSetUp2 = "C:/Users/Admin/Documents/Apps/chromedriver.exe";
+	public static final String Jenkins = "http://35.242.186.40:8080/";
+	
+	
 	
 
 }
