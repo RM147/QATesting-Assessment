3f05c5f227aa0895ed01faba2774386cb847a91c
Rana <mittrarana@gmail.com>
2019-03-01 11:34:23 +0000

    set up

---
+++ AutomatedTest/src/test/java/tests/AssessmentTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AssessmentTest {
+	
+	
+}
---
+++ AutomatedTest/src/test/java/tests/Constants.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Constants {
+	
+	public static final String EXCEL = "C:\\Users\\Admin\\Downloads\\Parametisation.xlsx";
+	public static final String DriverSetUp1 = "webdriver.chrome.driver";
+	public static final String DriverSetUp2 = "C:/Users/Admin/Documents/Apps/chromedriver.exe";
+	
+
+}
